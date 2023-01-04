ff09ba4f0648b66c9215c214c4cff90f48801efb
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-01-04 17:39:26 +0530

    ok
    ok

--- src/java/com/crud/project/ConnectionDao.java
+++ src/java/com/crud/project/ConnectionDao.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.crud.project;
-
+import java.sql.*;
 /**
  *
  * @author saifi
  */
 public class ConnectionDao {
-    
+    private static Connection con;
+
+    public static Connection getCon() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Book_management","root","root");
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        } 
+        return con;
+    }
 }
